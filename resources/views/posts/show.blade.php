c1de2e3df25680b415c6be9d3a4d17d3abd39438
InnervisionGit <ivan.felbabic@gmail.com>
2021-03-07 12:53:19 +0100

    going online

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,11 @@
+@extends("layouts.app")
+
+@section("content")
+    <a href="/posts" class="btn btn-outline-secondary" style="margin: 10px">Go back</a>
+    <h1>{{$post->title}}</h1>    
+    <div>
+        {{$post->body}}
+    </div>
+    <hr />
+    <small>Writen on {{$post->created_at}}</small>
+@endsection

631a0033dc62cc41090749e17a5513a820c8947d
InnervisionGit <ivan.felbabic@gmail.com>
2021-03-20 17:58:08 +0100

    edit posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,11 +1,13 @@
 @extends("layouts.app")
 
 @section("content")
-    <a href="/posts" class="btn btn-outline-secondary" style="margin: 10px">Go back</a>
+    <a href="/posts" class="btn btn-outline-secondary" style="margin: 10px 0px 10px 0px">Go back</a>
     <h1>{{$post->title}}</h1>    
     <div>
         {!! $post->body !!}
     </div>
     <hr />
     <small>Writen on {{$post->created_at}}</small>
+    <hr />
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-outline-secondary">Edit</a>
 @endsection

143f4d640f2ce29c16425a4e7ae90c81caf3e977
InnervisionGit <ivan.felbabic@gmail.com>
2021-03-22 19:11:22 +0100

    AUTH

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,25 +1,34 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
-        <title>{{config("app.name", "Laravel App")}}</title>
-        <script src="{{ asset('ckeditor/ckeditor.js') }}" defer></script>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    </head>
-    <body >
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        
         @include("inc.navbar")
         <div class="container">
             @include("inc.messages")
-            @yield("content")
+            <main class="py-4">
+                @yield('content')
+            </main>
         </div>
-
-        <script type="text/javascript">
-            $(document).ready(function () {    
-                $('.ckeditor').ckeditor();    
-            });    
-        </script>
-    
-    </body>
+    </div>
+</body>
 </html>
